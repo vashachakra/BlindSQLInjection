e6d7a41271dfd6a64c4a03c527c24c7fbdcbf1c3
vashachakra <115422203+vashachakra@users.noreply.github.com>
2023-12-06 23:09:13 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,56 +1,28 @@
+<?php
 
 if( isset( $_GET[ 'Submit' ] ) ) {
-    // Get input
-    $id = $_GET[ 'id' ];
-    $exists = false;
+        // Get input
+        $id = $_GET[ 'id' ];
 
-    switch ($_DVWA['SQLI_DB']) {
-        case MYSQL:
-            // Check database
-            $query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-            try {
-                $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ); // Removed 'or die' to suppress mysql errors
-            } catch (Exception $e) {
-                print "There was an error.";
-                exit;
-            }
+        // Check database
+        $getid  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+        $result = mysqli_query($GLOBALS["___mysqli_ston"],  $getid ); // Removed 'or die' to suppress mysql errors
 
-            $exists = false;
-            if ($result !== false) {
-                try {
-                    $exists = (mysqli_num_rows( $result ) > 0);
-                } catch(Exception $e) {
-                    $exists = false;
-                }
-            }
-            ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
-            break;
-        case SQLITE:
-            global $sqlite_db_connection;
+        // Get results
+        $num = @mysqli_num_rows( $result ); // The '@' character suppresses errors
+        if( $num > 0 ) {
+                // Feedback for end user
+                $html .= '<pre>User ID exists in the database.</pre>';
+        }
+        else {
+                // User wasn't found, so the page wasn't!
+                header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
 
-            $query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-            try {
-                $results = $sqlite_db_connection->query($query);
-                $row = $results->fetchArray();
-                $exists = $row !== false;
-            } catch(Exception $e) {
-                $exists = false;
-            }
+                // Feedback for end user
+                $html .= '<pre>User ID is MISSING from the database.</pre>';
+        }
 
-            break;
-    }
-
-    if ($exists) {
-        // Feedback for end user
-        echo '<pre>User ID exists in the database.</pre>';
-    } else {
-        // User wasn't found, so the page wasn't!
-        header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
-
-        // Feedback for end user
-        echo '<pre>User ID is MISSING from the database.</pre>';
-    }
-
+        ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
 
 ?>
